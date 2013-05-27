58f95738fea1e113bc04b5042100c2f42ff38595
Dahwar <dahwar2713@gmail.com>
2013-05-27 11:17:27 +0200

    Improve xmlLevelEncoder
    Improve xmlLevelEncoder

--- xmlLevelEncoder/src/xmlLevelEncoder/Main.java
+++ xmlLevelEncoder/src/xmlLevelEncoder/Main.java
@@ -2,6 +2,8 @@ package xmlLevelEncoder;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.jdom2.Attribute;
 import org.jdom2.Document;
@@ -11,42 +13,50 @@ import org.jdom2.output.XMLOutputter;
 
 public class Main
 {
-   static Element root = new Element("lvl");
+	static Element root = new Element("lvl");
+	static Document document = new Document(root);
+	static ArrayList<Element> eList = new ArrayList<>();
+	
+	public static void main(String[] args)
+	{
 
-   static Document document = new Document(root);
+		Attribute s = new Attribute("s","1");
+		root.setAttribute(s);		
+		
+		ObjectL oList = new ObjectL();
+		oList.add("0", "0");
+		oList.add("1", "1");
+		oList.add("2", "3");
+		Element e1 = new Element("e");
+		eList.add(putObjectIntoE(e1, oList));
+		
+		eList.add(new Element("e"));
+		
+		putElementIntoRoot();
+		
+		try
+		{
+			XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
+			sortie.output(document, System.out);
+			sortie.output(document, new FileOutputStream("lvl.xml"));
+		}
+		catch (IOException exc){}
+	}
 
-   public static void main(String[] args)
-   {
-	   
-	  Attribute s = new Attribute("s","1");
-	  root.setAttribute(s);
-	   
-      Element e1 = new Element("e");
-      root.addContent(e1);
-      
-      Element o1 = new Element("o");
-      e1.addContent(o1);
-      Attribute t1 = new Attribute("t","0");
-      Attribute l1 = new Attribute("l","0");
-      o1.setAttribute(t1);
-      o1.setAttribute(l1);
-      
-      Element o2 = new Element("o");
-      e1.addContent(o2);
-      Attribute t2 = new Attribute("t","1");
-      Attribute l2 = new Attribute("l","1");
-      o2.setAttribute(t2);
-      o2.setAttribute(l2);     
+	private static void putElementIntoRoot() {
+		for(Iterator i = eList.iterator(); i.hasNext();){
+			root.addContent((Element) i.next());
+		}
+	}
 
-      Element e2 = new Element("e");
-      root.addContent(e2);
-      
-      try
-      {
-         XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
-         sortie.output(document, System.out);
-         sortie.output(document, new FileOutputStream("lvl.xml"));
-      }
-      catch (IOException exc){}
-   }
+	private static Element putObjectIntoE(Element e1, ObjectL oList) {
+		for(Iterator i = oList.get().iterator(); i.hasNext();){
+			Pair pair = (Pair) i.next();
+			Element o = new Element("o");
+			o.setAttribute(new Attribute("t",pair.getFirst()));
+			o.setAttribute(new Attribute("l",pair.getSecond()));
+			e1.addContent(o);
+		}
+		return e1;
+	}
 }
